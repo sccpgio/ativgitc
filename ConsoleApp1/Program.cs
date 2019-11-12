e15ce8d2d887369e2964aaf71b59937991ef40e2
0mariaragos <mariana.aragos@etec.sp.gov.br>
2019-11-11 23:27:16 -0200

    primeiro commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static int somar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
