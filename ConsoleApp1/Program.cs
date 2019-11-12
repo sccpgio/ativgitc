f39f8f3151b522dcbca9216295274525ae661d29
marcelomartins123 <marcelomartins1229@gmail.com>
2019-11-11 23:40:46 -0200

    é isso ai

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,10 @@ namespace ConsoleApp1
         {
             return valor1 + valor2;
         }
+        public static int subtracao(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
         static void Main(string[] args)
         {
 
